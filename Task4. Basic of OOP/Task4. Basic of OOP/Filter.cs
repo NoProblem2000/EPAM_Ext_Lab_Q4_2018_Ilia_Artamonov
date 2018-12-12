6822f96272804909840b692e34a06a677250d4f6
Art <fernando94@yandex.ru>
2018-12-12 20:17:43 +0400

    Add FilterClass and Roles

---
+++ Task4. Basic of OOP/Task4. Basic of OOP/Filter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Task4.Basic_of_OOP
+{
+    public class Filter
+    {
+        public string Style;
+        public string Actor;
+        public string NameFilm;
+        public string FilmMaker;
+        public string Mark;
+        public string DateOfPublishing;
+
+        public DetailsFilm DetailsFilm
+        {
+            get => default(DetailsFilm);
+            set
+            {
+            }
+        }
+    }
+}
---
+++ Task4. Basic of OOP/Task4. Basic of OOP/Roles.cs
@@ -0,0 +1,15 @@
+﻿namespace Task4.Basic_of_OOP
+{
+    public class Roles
+    {
+        private string Role;
+
+        public Person Person
+        {
+            get => default(Person);
+            set
+            {
+            }
+        }
+    }
+}
